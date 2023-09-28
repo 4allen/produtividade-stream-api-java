bb0807ed4c1d8d8b9ccc769dc06b1835bb788af7
hellen atanasio <l.atanasio@hotmail.com>
2023-09-28 17:45:16 -0300

    Functional Interface Example

---
+++ functional-interface/src/BinaryOperatorExample.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+  public static void main(String[] args) {
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+    BinaryOperator<Integer> somar = new BinaryOperator<Integer>() {
+      @Override
+      public Integer apply(Integer num1, Integer num2) {
+        return num1 + num2;
+      }
+    };
+    int resultado = numeros.stream().reduce(0, somar);
+    System.out.println("A soma dos números é: " + resultado);
+  }
+}
